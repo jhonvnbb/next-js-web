b76eb3835cedbd454ed291c71af93f9a75ca7184
jhonvnbb <tinonababan3@gmail.com>
2024-08-16 19:04:31 +0700

    create:banner

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -3,14 +3,27 @@ import Link from "next/link";
 
 const BannerSection = () => {
   return (
-    <section className="bg-gray-800">
-      <div className="container mx-auto flex items-center justify-around h-[100vh] bg-[#333]">
-        <div>
-          <h1>Dare to Dream Strive for Greatness</h1>
-          <Link href="">Get Started</Link>
+    <section className="bg-[#eee] py-24">
+      <div className="container mx-auto flex flex-col items-center justify-center p-6">
+        <div className="w-full md:w-1/2 flex justify-center mb-6 md:mb-0">
+          <img
+            src="/himakom.png"
+            alt="logo-himakom"
+            className="max-w-full h-auto"
+          />
         </div>
-        <div>
-            <img src="/himakom.png" alt="logo-himakom" />
+        <div className="w-full md:w-1/2 text-center">
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-800">
+            Dare to Dream, Strive for Greatness
+          </h1>
+          <div className="mt-6">
+            <Link
+              href=""
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 border rounded inline-block"
+            >
+              Get Started
+            </Link>
+          </div>
         </div>
       </div>
     </section>
--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -12,7 +12,7 @@ const NavigationBar = () => {
   };
 
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-6">
       <div className="container mx-auto flex justify-between items-center px-4">
         <div className="text-white text-lg font-bold">
           <Link href="/">HIMAKOM</Link>
